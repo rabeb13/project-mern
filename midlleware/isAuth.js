00e6c890d15540a55ef306ad1077a6391cc87570
rabeb messaoudi <rabebmessaoudi123@gmail.com>
2025-07-24 00:20:10 +0200

    second

--- midlleware/isAuth.js
+++ midlleware/isAuth.js
@@ -4,19 +4,19 @@ const User = require('../models/User');
 const isAuth = async (req, res, next) => {
     try {
         // token => headers
-        const token = req.headers['Authorization'];
+        const token = req.headers['authorization'];
         if (!token) {
-            return res.status(401).send({ msg: "You are not authorized" });
+            return res.status(401).send({ msg: "You are not authorized 1" });
         }
         const decoded = jwt.verify(token, process.env.SECRET_KEY);
     const foundUser = await User.findOne({_id: decoded.id});
     if (!foundUser) {
-        return
+        return res.status(401).send({ msg: "You are not authorized 2" });
     }
     req.user = foundUser;
     next();
 } catch (error) {
-        res.status(401).send({ msg: "You are not authorized", error });
+        res.status(401).send({ msg: "You are not authorized 3", error });
     }
 };
 module.exports = isAuth;
